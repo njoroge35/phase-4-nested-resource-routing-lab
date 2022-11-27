f97203620ded8aeecce657efc72e42c3c8e94b02
David <david.njoroge@student.moringaschool.com>
2022-11-27 07:39:25 +0300

    completed nested resources lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+
+  end
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
